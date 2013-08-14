2a7873e77ac392c32ae821204346110167d84045
Luthra <nluthra@MTVL11325ff64.local>
2013-08-14 13:42:31 -0700

    ia working

---
+++ IntuitAnywhere/src/main/java/users/nluthra/NLTest1.java
@@ -0,0 +1,11 @@
+package users.nluthra;
+
+import users.nluthra.customer.CustomerFactory;
+
+public class NLTest1 {
+
+	public static void main(String[] args) {
+		CustomerFactory.printCustomers();
+	}
+
+}
